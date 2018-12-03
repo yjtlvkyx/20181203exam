b9c355899c90c309d6f79ab224a6fad64d6145ad
yjtlvkyx <756994208@qq.com>
2018-12-03 09:23:43 +0800

    编译sass成功

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 const gulp = require("gulp"),
     sass = require("gulp-sass"),
-    server = require("gulp-webserver");
+    server = require("gulp-webserver"),
+    cleanCss = require("gulp-clean-css");
 
-//启服务 //成功
+//编译sass文件  //成功
+gulp.task("devSass", () => {
+    return gulp.src("./src/scss/*.scss").pipe(sass()).pipe(cleanCss()).pipe(gulp.dest("./src/css"));
+})
+
+//启服务  //成功
 gulp.task("server", () => {
     return gulp.src("./src/").pipe(server({
         port: 8024,
